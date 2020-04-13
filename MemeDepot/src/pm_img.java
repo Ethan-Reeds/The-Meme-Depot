abfa4599b5dc4fb39fe345d0e48751111cf3d9d7
Ethan Reeds <user@192.168.254.39>
2020-04-13 11:15:03 -0400

    added new servlet, for handling the file messages, nothing is done on it quite yet, also added new function in jQuery to call that servlet and pass correct information.

---
+++ MemeDepot/src/pm_img.java
@@ -0,0 +1,59 @@
+
+/**
+ *
+ * @author Ethan Reeds/Jim
+ * Ripped this from the example in class
+ */
+
+import java.io.IOException;
+import java.util.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns={"/pm_img"})
+@MultipartConfig
+public class pm_img extends HttpServlet{
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        
+        // right now this is the pm_txt code, the intent is to edit this into handling a file
+        resp.setContentType("text/plain");
+        var pw = resp.getWriter();
+        
+        var from = req.getParameter("from"); 
+        var to = req.getParameter("to");     // handle input errors with jquery
+        var msg = req.getParameter("message");
+        
+        // error handling
+        if (from == null || to == null)
+            pw.println("Username(s) missing");    // this is for testing purposes
+            // might be able to handle this in the jquery 
+        else if (msg == null)
+            pw.println("No message");
+        else{
+            Account a = AccountManager.getAccount(from);
+            Account b = AccountManager.getAccount(to);
+            if(a == null || b == null)
+                pw.println("Account(s) do not exist");    // makes sure accounts are valid
+            else{
+                Message msgObj = new Message(msg, a, b);
+                messageManager.addMessage(msgObj);
+
+                //getting the message list back to display
+                ArrayList<Message> mList = new ArrayList<>();
+                mList = messageManager.getMessages(a, b);
+
+
+                for (Message m : mList){
+                    pw.println(m.message);
+                }
+            }
+        }
+        
+        
+    }
+}
--- MemeDepot/webapps/ROOT/js/personal_message.js
+++ MemeDepot/webapps/ROOT/js/personal_message.js
@@ -1,13 +1,14 @@
 "use strict";
 
 function sendMessage(){
-    let to = $("#username").val();
-    // from will be handled by the servlet by checking the session to see who is logged in
+    let to = $("#username_to").val();
+    let from = $("#username_from").val();
     let message = $("#message").val();
     
     let fdata = new FormData();
 
     fdata.append("to",to);
+    fdata.append("from", from);
     fdata.append("message",message);
 
     $.ajax({
@@ -18,6 +19,37 @@ function sendMessage(){
         processData: false,
         success: (data) => {
             $("#statusdiv").html("Response: "+data);
+        },
+        error: (xhr,status,err) => {
+            $("#statusdiv").html("Something went wrong: status="+status+" err="+err);
+        }
+
+    });
+
+}
+
+"use strict";
+
+function sendImage(){
+
+    let to = $("#username_to").val();
+    let from = $("#username_from").val();
+    let message = $("#message").prop("files")[0];
+    
+    let fdata = new FormData();
+
+    fdata.append("to",to);
+    fdata.append("from", from);
+    fdata.append("message",message);
+
+    $.ajax({
+        type: "POST",
+        url: "/srv/pm_txt",
+        data: fdata,
+        contentType: false, 
+        processData: false,
+        success: (data) => {
+            $("#statusdiv").html("Server said this: "+data);
         },
         error: (xhr,status,err) => {
             $("#statusdiv").html("Something went wrong: status="+status+" err="+err);
