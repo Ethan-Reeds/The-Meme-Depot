2a99557e4f15c3c26ecac89fe689e2bb423f9e91
Hhawkins2520 <hhawkins2520@gmail.com>
2020-05-04 22:30:46 -0400

    created for DRY code

---
+++ MemeDepot/src/SQLAdminInfo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hhawk
+ */
+public class SQLAdminInfo {
+    static public String url = "jdbc:mysql://localhost:3306/memedepot?"
+            + "autoReconnect=true&useSSL=false";
+    static public String driver = "com.mysql.jdbc.Driver";
+    static public String user = "root";
+    static public String password = "HhLlHh@972";
+}
